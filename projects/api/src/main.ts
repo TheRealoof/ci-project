80038b51088d0395bf354694d5d50ae5dab7c777
TheRealOof <grassetc31@gmail.com>
2025-02-09 15:44:01 +0100

    Split api files

--- projects/api/src/main.ts
+++ projects/api/src/main.ts
@@ -8,6 +8,8 @@ import * as path from 'path';
 import environment from './environment';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import auth from './routes/auth';
+import user from './routes/user';
 
 const app = express();
 
@@ -30,82 +32,16 @@ const verifyToken = (req: express.Request, res: express.Response, next: express.
   next();
 }
 
-app.get('/api', (req, res) => {
+const router = express.Router();
+
+app.use('/api', router);
+
+router.get('/', (_req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
 
-app.get('/api/auth', async (req, res) => {
-  if (!req.query["redirect_uri"]) {
-    res.status(400).send({ error: "redirect_uri is required" });
-    return;
-  }
-  const redirectUri = encodeURIComponent(req.query["redirect_uri"] as string);
-  const clientId = environment.discordClientId;
-  const scope = "guilds+identify";
-  const url = `https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scope}`;
-  res.send({ url: url });
-});
-
-app.post('/api/auth/token', async (req, res) => {
-  if (!req.query["code"]) {
-    res.status(400).send({ error: "code is required" });
-    return;
-  }
-  if (!req.query["redirect_uri"]) {
-    res.status(400).send({ error: "redirect_uri is required" });
-    return;
-  }
-
-  const clientId = environment.discordClientId;
-  const clientSecret = environment.discordClientSecret;
-  const code = req.query["code"] as string;
-  const redirectUri = req.query["redirect_uri"] as string;
-  const params = new URLSearchParams({
-    client_id: clientId,
-    client_secret: clientSecret,
-    grant_type: "authorization_code",
-    code: code,
-    redirect_uri: redirectUri,
-  });
-  const url = `https://discord.com/api/oauth2/token`;
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Accept-Encoding': 'application/x-www-form-urlencoded',
-    },
-    body: params,
-  });
-  const json = await response.json();
-  console.log(json);
-
-  const user = await fetch('https://discord.com/api/users/@me', {
-    headers: {
-      'Authorization': `Bearer ${json.access_token}`,
-    }
-  });
-
-  console.log(await user.json());
-
-  res.cookie('token', json.access_token, { httpOnly: true });
-  res.send({ token: json });
-});
-
-app.get('/api/auth/logout', async (req, res) => {
-  res.clearCookie('token');
-  res.send({ message: 'Logged out' });
-});
-
-app.get('/api/user', verifyToken, async (req, res) => {
-  const token: string = req["token"];
-  const url = 'https://discord.com/api/users/@me';
-  const response = await fetch(url, {
-    headers: {
-      'Authorization': `Bearer ${token}`,
-    }
-  });
-  res.send(await response.json());
-});
+router.use('/auth', auth);
+router.use('/user', verifyToken, user);
 
 const server = app.listen(environment.port, () => {
   console.log(`Listening at ${environment.apiBaseUrl}`);
